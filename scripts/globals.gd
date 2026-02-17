56b656a6cb987ba642472ba32b40a3d636b59361
fawfle <0neofthoseguys123@gmail.com>
2026-02-16 22:20:27 -0700

    core guest gameplay prototype

--- scripts/globals.gd
+++ scripts/globals.gd
@@ -7,6 +7,9 @@ var DEBUG: bool = true
 ## used to signal textbox to display a mesage. Message is given as an array and clicking through will cycle until final message.
 @warning_ignore("unused_signal")
 signal set_text(lines: Array[String])
+## emited when text has been cycled through to end of queue
+@warning_ignore("unused_signal")
+signal text_finished()
 
 ## used by rooms to signal a click.
 @warning_ignore("unused_signal")
