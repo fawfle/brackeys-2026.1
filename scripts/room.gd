55cdfae90daf5d72fad3d50038c1809c0c946902
Sackboy495 <sackmike2007@icloud.com>
2026-02-18 11:23:59 -0700

    something or another

--- scripts/room.gd
+++ scripts/room.gd
@@ -23,7 +23,6 @@ func add_guest(_guest: Guest):
 
 func _on_texture_button_button_down() -> void:
 	Globals.select_room.emit(self)
-	$Upgrade_Scene.visible = true
 
 func get_happiness_rating() -> float:
 	var rating: float = guest.initial_rating
